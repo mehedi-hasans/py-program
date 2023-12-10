1afe0b265dc5c1005d52b9be26d9720905db57b2
Mehedi Hasan <mehedi4595@gmail.com>
2023-12-10 12:46:43 +0600

    Another program

--- function.py
+++ function.py
@@ -4,3 +4,37 @@
 # name = str(input('Enter your name: ')).title().strip()
 # # name = str(input('Enter your name: ')).title().capitalize()
 # hello(name)
+
+
+
+# def main():
+#     name = input('Enter your name: ')
+#     hello(name)
+
+# def hello(s):
+#     print('hello', s)
+
+# main()
+
+
+def main():
+    x = int(input('Enter your value: '))
+    print('X is squire is: ', squire(x))
+
+def squire(n):
+    # return n*n
+    return pow(n, 2)
+
+
+main()
+
+
+def main():
+    x = int(input('Enter your value: '))
+    print('X is squire is: ', squire(x))
+
+def squire(n):
+    # return n*n
+    return pow(n, 2)
+
+main()
